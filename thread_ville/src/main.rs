f06e2422ecada9c1413ff68733dfcfe0b2c9e46e
JASCH97 <joansc16@outlook.com>
2022-05-22 23:28:54 -0600

    Se agregan casos de prueba del codigo

--- thread_ville/src/main.rs
+++ thread_ville/src/main.rs
@@ -2,8 +2,27 @@ mod thread_city;
 
 
 fn main() {
-    let mut m = thread_city::create_city_matrix();
+    //replicas de la ciudad
+    let city = thread_city::create_city_matrix();
+    let city2 = thread_city::create_city_matrix();
+    let city3 = thread_city::create_city_matrix();
 
-    thread_city::find_path(6,4,0,4,m);
+    //prueba con un carro
+    let add_car = thread_city::add_car_to_city(city);
+    thread_city::follow_path(add_car, city2,2);
+
+    //prueba con un barco
+    //let add_ship = thread_city::add_ship_to_city();
+    //thread_city::follow_path(add_ship, city3,4);
+
+    //prueba con una ambulancia
+    //let add_car = thread_city::add_car_to_city(city);
+    //thread_city::follow_path(add_car, city2,4);
+
+
+    /*NOTA: Se puede probar con un carro y barco o ambulancia y barco a la vez
+            pero no se puede carro y ambulancia a la vez por problemas de 
+            apropiamiento de rust en cuanto a variables/funciones
+    */
 
 }
